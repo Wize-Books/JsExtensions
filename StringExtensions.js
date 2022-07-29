a8463b38d15cc6e3f94d68e18ca41bfc06879fcf
Gideon Scheepers <dot.kiddo@gmail.com>
2022-07-29 10:55:12 +0200

    - changed to iife

--- StringExtensions.js
+++ StringExtensions.js
@@ -1,3 +1,7 @@
-if (String.prototype.parseMSNetMVCJsonDate != undefined){
-    String.prototype.parseMSNetMVCJsonDate = function() { return new Date(Number(this.match(/\d+/))); }
-}
+(function(){
+
+    if (String.prototype.parseMSNetMVCJsonDate != undefined){
+        String.prototype.parseMSNetMVCJsonDate = function() { return new Date(Number(this.match(/\d+/))); }
+    }
+
+})();
